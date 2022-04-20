90b73bbf7af7e18102ab53a47ac7f3c316eb3f5f
Justin Mayfield <tooker@gmail.com>
2022-04-20 16:48:13 -0600

    Super simple example of sauce for zwfit ws api in python

---
+++ example.py
@@ -0,0 +1,54 @@
+import json
+import random
+import sys
+import websocket
+
+request_id = f'random-req-id-{random.randint(1, 100000000)}'
+sub_id = f'random-sub-id-{random.randint(1, 100000000)}'
+
+def on_message(ws, raw_msg):
+    msg = json.loads(raw_msg)
+    if msg['type'] == 'response':
+        if not msg['success']:
+            raise Exception('subscribe request failure')
+    elif msg['type'] == 'event' and msg['success']:
+        data = msg['data']
+        print()
+        print(json.dumps(data, sort_keys=True, indent=4))
+
+
+def on_error(ws, error):
+    print("socket error", error)
+
+
+def on_close(ws, status_code, msg):
+    print("socket closed", status_code, msg)
+
+
+def on_open(ws):
+    ws.send(json.dumps({
+        "type": "request",
+        "uid": request_id,
+        "data": {
+            "method": "subscribe",
+            "arg": {
+                "event": "watching", # watching, nearby, groups, etc...
+                "subId": sub_id
+            }
+        }
+    }))
+
+
+if __name__ == "__main__":
+    #websocket.enableTrace(True)
+    if len(sys.argv) < 2:
+        host = "ws://localhost:1080/api/ws"
+    else:
+        host = sys.argv[1]
+    print("Connecting to:", host)
+    ws = websocket.WebSocketApp(host,
+                                on_open = on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.run_forever()
